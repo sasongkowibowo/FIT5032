58699b994fd71daea37f851df3c355125f1dea8f
Sasongko Wibowo <swib0002@student.monash.edu>
2019-08-08 19:06:59 +1000

    Add project files.

---
+++ IndonesianTaste/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IndonesianTaste.MvcApplication" Language="C#" %>
---
+++ IndonesianTaste/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndonesianTaste.Startup))]
+namespace IndonesianTaste
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
